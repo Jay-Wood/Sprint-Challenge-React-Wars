de2a3447de22d92a364945fb02f3b14b9c404f40
Jay Wood <47437698+jaywood8@users.noreply.github.com>
2019-07-19 09:32:37 -0700

    Styled for 10 chars, looking back for additional chars

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import './App.css';
 import PersonCard from "./components/peopleCard.js";
+import styled from "styled-components";
 
 const App = () => {
   /* Try to think through what state you'll need for this app before starting. Then build out the state properties here. Fetch characters from the star wars api in an effect hook. Remember, anytime you have a side effect in a component, you want to think about which state and/or props it should
@@ -23,15 +24,16 @@ const App = () => {
     
     <div className="App"> 
       <h1 className="Header">React Wars</h1>
-
-      {dataObj.map(char => ( <PersonCard 
-        name = {char.name}
-        born = {char.birth_year}
-        key = {char.name}
-        gender = {char.gender}
-        homeworld = {char.homeworld}
-      />
+      <div className="CardContainer">
+        {dataObj.map(char => ( <PersonCard 
+          name = {char.name}
+          born = {char.birth_year}
+          key = {char.name}
+          gender = {char.gender}
+          // homeworld = {char.homeworld}
+        />
       ))} 
+      </div>
     </div>
   );
 
